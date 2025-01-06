d3fe245fe92a19ccba89376b05374124003f0a01
regina-phalange-dev <kinza2592@gmail.com>
2025-01-07 04:24:33 +0500

    Add files via upload

---
+++ ebill/user/footer.php
@@ -0,0 +1,8 @@
+<!-- FOOTER -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                Created by <a href="">WattGuard </a> Systems
+            </div>
+        </div>
+    </footer>
